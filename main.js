2bcaf30af1b1f3537ebf6b39e7945cdc168c3b5d
dorinel.js <38764706+dorinelrushi@users.noreply.github.com>
2020-05-11 21:12:00 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var box=document.querySelector("#page");
+var times=document.querySelector("#times");
+var bars=document.querySelector("#bars");
+//page none
+times.addEventListener("click",function12);
+function function12(){
+  box.style.display="none";
+}
+//page show 
+bars.addEventListener("click",create)
+function create(){
+  box.style.display="block";
+}
